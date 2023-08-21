b33f03eb2c71aa5273e041e5dceb01b0965716c3
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-08-21 22:47:27 +0100

    Added functionalities

---
+++ script.js
@@ -0,0 +1,22 @@
+const quoteNumber = document.querySelector('#quoteNumber');
+const quoteParagraph = document.querySelector('#quoteParagraph');
+const quoteBtn = document.querySelector('#quoteBtn');
+
+getQuote = (id, advice) => {
+    quoteNumber.textContent = id;
+    quoteParagraph.textContent = advice;
+}
+
+generateQuote = () => {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(quote => {
+        getQuote(quote.slip["id"], quote.slip["advice"]);
+    })
+}
+
+generateQuote();
+
+quoteBtn.addEventListener('click', async () => {
+    setTimeout(generateQuote, 1000)
+})
